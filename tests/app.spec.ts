e9ae13629822dbbb704fc97063c86edb895b5e18
sptm <me@sptm.dev>
2023-07-08 14:54:09 +0400

    [SPT-13] add placeholder test to generate coverage report

---
+++ tests/app.spec.ts
@@ -0,0 +1,11 @@
+describe('App', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('placeholder test', () => {
+    it('should always pass', () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
